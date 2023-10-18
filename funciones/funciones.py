c85735f7616f0b76cc7220ea05ff85f907a42523
Pablo <alu0101398327@ull.edu.es>
2023-10-18 13:16:39 +0100

    Entrada bien

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 # Función que lee las palabras de parada de un archivo
 def leer_archivo_palabras_parada(nombre_archivo_palabras_parada):
@@ -11,18 +12,26 @@ def leer_archivo_palabras_parada(nombre_archivo_palabras_parada):
 
 # Función que lee el texto
 def leer_texto(nombre_archivo_documentos):
+    # Inicializa un vector vacío para almacenar los vectores de palabras
+    vector_de_lineas = []
+
+    # Abre el archivo y lee línea por línea
     with open(nombre_archivo_documentos, 'r') as archivo:
-        lineas = archivo.readlines()
-        texto = []
-        for linea in lineas:
-            texto.append(linea)
-    return texto
+        for linea in archivo:
+            # Divide la línea en palabras
+            palabras = linea.split()
+            # Agrega el vector de palabras al vector principal
+            vector_de_lineas.append(palabras)
+
+    return vector_de_lineas
 
 # Función que elimina las palabras de parada de un texto
 def eliminar_palabras_parada(texto, palabras):
     for i in palabras:
-        for j in range(len(texto)):
-            texto[j] = texto[j].replace(i, '')
+        for j in texto:
+            for k in j:
+                if i == k:
+                    j.remove(k)
     return texto
 
 # Función que lee el archivo lematización
@@ -32,10 +41,15 @@ def leer_archivo_lematizacion(nombre_archivo_lematizacion):
         texto_json = json.loads(texto)
     return texto_json
 
-
 # Función que cambia las palabras por otras dadas
 def cambiar_palabras(texto, lematizacion):
-    for i in lematizacion: # Falla en este bucle
-        for j in range(len(texto)):
-            texto[j] = texto[j].replace(i, lematizacion[i])
+    for palabra_original, palabra_lematizada in lematizacion.items():
+        for i in range(len(texto)):
+            for j in range(len(texto[i])):
+                if texto[i][j] == palabra_original:
+                    texto[i][j] = palabra_lematizada
     return texto
+
+    
+
+
