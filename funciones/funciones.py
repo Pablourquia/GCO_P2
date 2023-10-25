42782844364660be791486b2c7a6058426c09156
Pablo <alu0101398327@ull.edu.es>
2023-10-25 11:42:32 +0100

    Finalizada

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -1,5 +1,6 @@
 import json
 import re
+import math
 
 # Función que lee las palabras de parada de un archivo
 def leer_archivo_palabras_parada(nombre_archivo_palabras_parada):
@@ -59,7 +60,7 @@ def calcular_TF(termino, documento):
     return tf / len(documento)
 
 # Funcion que calcula el IDF de un término
-def calculat_IDF(termino, documentos):
+def calcular_IDF(termino, documentos):
     df = 0
     for i in documentos:
         for j in i:
@@ -70,7 +71,7 @@ def calculat_IDF(termino, documentos):
 
 # Funcion que calcula el TF-IDF de un término
 def calcular_TF_IDF(termino, documento, documentos):
-    return calcular_TF(termino, documento) * calculat_IDF(termino, documentos)
+    return calcular_TF(termino, documento) * calcular_IDF(termino, documentos)
 
 # Función que calcula el índice del término
 def calcular_indice_termino(termino, documento):
@@ -80,7 +81,37 @@ def calcular_indice_termino(termino, documento):
             indice.append(i)
     return indice
 
+def buscar_palabra(termino, tabla):
+    for i in range(len(tabla)):
+        if tabla[i][1] == termino:
+            return i
+    return -1
 
-    
+# Función que para cada documento me va a crear una tabla con:
+# Índice del término, término, TF, IDF, TF-IDF
+def crear_tabla(documentos, documento):
+    tabla = []
+    for i in documento:
+        if buscar_palabra(i, tabla) == -1:
+            array = []
+            array.append(calcular_indice_termino(i, documento))
+            array.append(i)
+            array.append(calcular_TF(i, documento))
+            array.append(calcular_IDF(i, documentos))
+            array.append(calcular_TF_IDF(i, documento, documentos))
+            tabla.append(array)
+    return tabla
 
+# Función que ordena las tablas por el IDF y devuelve los 5 mejores
+def ordenar_mejores_idf(tabla):
+    tabla.sort(key=lambda x: x[3], reverse=True)
+    return tabla[:5]
 
+# Calcula la similitud coseno entre varias tablas
+def calcular_similitud_coseno(tabla1, tabla2):
+    similitud = 0
+    if len(tabla1) != len(tabla2):
+        return -1
+    for i in range(len(tabla1)):
+        similitud += tabla1[i][4] * tabla2[i][4]
+    return similitud
--- main.py
+++ main.py
@@ -32,4 +32,25 @@ texto_inicial = leer_texto(nombre_archivo_documentos)
 texto = eliminar_palabras_parada(texto_inicial, palabras)
 lematizacion = leer_archivo_lematizacion(nombre_archivo_lematizacion)
 texto_final = cambiar_palabras(texto, lematizacion)
-print(calcular_similaridad_coseno(texto_final[0], texto_final[1]))
+
+array_tablas = []
+
+with open(nombre_archivo_salida, 'w') as archivo:
+    # Imprimir las tablas de frecuencia de términos
+    for i in range(len(texto_final)):
+        archivo.write('Documento ' + str(i + 1) + "\n")
+        tabla = crear_tabla(texto_final, texto_final[i])
+        archivo.write("Índice del término\tTérmino\tTF\tIDF\tTF-IDF \n")
+        for i in range(len(tabla)):
+            archivo.write(str(tabla[i][0]) + "\t" + str(tabla[i][1]) + "\t" + str(round(tabla[i][2],5)) + "\t" + str(round(tabla[i][3],5)) + "\t" + str(round(tabla[i][4],5)) + "\n")
+        archivo.write("\n")
+        array_tablas.append(ordenar_mejores_idf(tabla))
+    
+    # Imprimir la similitud coseno entre las tablas
+    archivo.write('Similitud coseno entre los documentos \n')
+    for i in range(len(array_tablas)):
+        for j in range(i + 1, len(array_tablas)):
+            archivo.write('Similitud coseno entre el documento ' + str(i + 1) + ' y el documento ' + str(j + 1) + ': ' + str(round(calcular_similitud_coseno(array_tablas[i], array_tablas[j]),5)) + '\n')
+
+
+    
